d3aa31b00ed5828ec504d84f6fdc8b5291047e64
Seth Kerr <41877068+skerr92@users.noreply.github.com>
2019-04-27 14:51:45 -0400

    Create custom.js

---
+++ webbed/js/custom.js
@@ -0,0 +1,5 @@
+/**
+* This is our custom JavaScript File! We will add our code here! 
+* Please note, that this is just a practice file
+* Refer to the README.md file for the webbed section of the codestream repository!
+*/
